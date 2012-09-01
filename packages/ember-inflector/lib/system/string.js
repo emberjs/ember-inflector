f66bbccab456138fbddf7afdc307ae254c9d2f3b
Karl Guillotte <karl.guillotte.1@gmail.com>
2012-09-01 00:29:29 -0700

    Add Ember.String methods to String.prototype
    Methods pluralize, singularize, humanize, titleize, capitalize,
    tableize and classify from Ember.String are now available on
    String.prototype.

--- packages/ember-inflector/lib/system/string.js
+++ packages/ember-inflector/lib/system/string.js
@@ -1,10 +1,17 @@
 Ember.String.pluralize = function(word) {
   return Ember.Inflector.inflect(word, Ember.Inflector.rules.plurals);
 };
+String.prototype.pluralize = function() {
+  return Ember.String.pluralize(this, arguments);
+};
 
 Ember.String.singularize = function(word) {
   return Ember.Inflector.inflect(word, Ember.Inflector.rules.singular);
 };
+String.prototype.singularize = function() {
+  return Ember.String.singularize(this, arguments);
+};
+
 
 Ember.String.humanize = function(word) {
 
@@ -18,6 +25,9 @@ Ember.String.humanize = function(word) {
 
   return Ember.String.capitalize(inflected);
 };
+String.prototype.humanize = function() {
+  return Ember.String.humanize(this, arguments);
+};
 
 Ember.String.titleize = function(word) {
    var result = Ember.String.humanize(word);
@@ -29,17 +39,29 @@ Ember.String.titleize = function(word) {
 
   return result;
 };
+String.prototype.titleize = function() {
+  return Ember.String.titleize(this, arguments);
+};
 
 Ember.String.capitalize = function(word) {
   return word.replace(Ember.Inflector.FIRST_LETTER_REGEX, function(match) {
     return match.toUpperCase();
   });
 };
+String.prototype.capitalize = function() {
+  return Ember.String.capitalize(this, arguments);
+};
 
 Ember.String.tableize = function(word) {
   return Ember.String.pluralize(Ember.String.underscore(word.toLowerCase()));
 };
+String.prototype.tableize = function() {
+  return Ember.String.tableize(this, arguments);
+};
 
 Ember.String.classify = function(word) {
   return Ember.String.capitalize(Ember.String.camelize(Ember.String.singularize(word)));
+};
+String.prototype.classify = function() {
+  return Ember.String.classify(this, arguments);
 };
