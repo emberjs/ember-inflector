9c8068b981d861cb95124eccfaf39d3445871d05
Karl Guillotte <karl.guillotte.1@gmail.com>
2012-09-04 10:28:53 -0700

    Separate prototype extension into ext string.js

--- packages/ember-inflector/lib/system.js
+++ packages/ember-inflector/lib/system.js
@@ -2,3 +2,4 @@ require('ember-inflector/system/string');
 require('ember-inflector/system/inflector');
 require('ember-inflector/system/inflections');
 require('ember-inflector/ext/ordinalization');
+require('ember-inflector/ext/string');
