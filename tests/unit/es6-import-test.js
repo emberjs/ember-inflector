b169434d217defb8f9359a265aada1764ab5ae17
Manuel Wiedenmann <manuel@funkensturm.de>
2015-03-15 20:33:12 +0100

    add es6 import test

---
+++ tests/unit/es6-import-test.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import {
+  module,
+  test
+} from 'qunit';
+import Inflector from 'ember-inflector';
+import {singularize, pluralize} from 'ember-inflector';
+
+module('ember-inflector.dsl imports');
+
+test('es6 import', function(assert) {
+  assert.equal(Inflector.inflector.pluralize('cow'), 'kine', 'no pluralization rule');
+  assert.equal(pluralize('cow'), 'kine', 'no pluralization rule');
+  
+  assert.equal(Inflector.inflector.singularize('kine'), 'cow', 'no singularization rule');
+  assert.equal(singularize('kine'), 'cow', 'no singularization rule');
+});
