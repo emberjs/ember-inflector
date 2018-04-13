aec79fd6476a9740a8b69da9c3c7ef595d692c74
Keita Urashima <ursm@ursm.jp>
2018-04-13 13:17:41 +0900

    Avoid using legacy global flags
    Several flags (including EXTEND_PROTOTYPES) underneath the Ember namespace have been removed in Ember 3.2.
    https://github.com/emberjs/ember.js/pull/16467

--- addon/lib/ext/string.js
+++ addon/lib/ext/string.js
@@ -4,7 +4,7 @@ import {
   singularize
 } from '../system/string';
 
-if (Ember.EXTEND_PROTOTYPES === true || Ember.EXTEND_PROTOTYPES.String) {
+if (Ember.ENV.EXTEND_PROTOTYPES === true || Ember.ENV.EXTEND_PROTOTYPES.String) {
   /**
     See {{#crossLink "Ember.String/pluralize"}}{{/crossLink}}
 
