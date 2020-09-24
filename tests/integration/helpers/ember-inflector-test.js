c3eb10cb3b405ad4ae79b1428afa943a21a9e1b7
Nathaniel Furniss <nlfurniss@gmail.com>
2020-09-23 22:33:28 -0700

    Upgrade ember-cli-babel to v7

--- tests/integration/helpers/ember-inflector-test.js
+++ tests/integration/helpers/ember-inflector-test.js
@@ -1,135 +1,137 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ember-inflector', 'Integration | Helpers', {
-  integration: true
-});
+module('Integration | Helpers', function(hooks) {
+  setupRenderingTest(hooks);
 
-test("singularize", function(assert) {
-  this.set('plural', 'octopi');
-  this.render(hbs`{{singularize plural}}`);
-  assert.equal(this.$().text(), 'octopus');
-});
+  test("singularize", async function(assert) {
+    this.set('plural', 'octopi');
+    await render(hbs`{{singularize plural}}`);
+    assert.equal(this.$().text(), 'octopus');
+  });
 
-test("pluralize - single arg", function(assert) {
-  this.set('singular', 'ox');
-  this.render(hbs`{{pluralize singular}}`);
-  assert.equal(this.$().text(), 'oxen');
-});
+  test("pluralize - single arg", async function(assert) {
+    this.set('singular', 'ox');
+    await render(hbs`{{pluralize singular}}`);
+    assert.equal(this.$().text(), 'oxen');
+  });
 
-test("pluralize - count 1", function(assert) {
-  this.set('singular', 'opossum');
-  this.render(hbs`{{pluralize 1 singular}}`);
-  assert.equal(this.$().text(), '1 opossum');
-});
+  test("pluralize - count 1", async function(assert) {
+    this.set('singular', 'opossum');
+    await render(hbs`{{pluralize 1 singular}}`);
+    assert.equal(this.$().text(), '1 opossum');
+  });
 
-test("pluralize - count 2", function(assert) {
-  this.set('singular', 'ocelot');
-  this.render(hbs`{{pluralize 2 singular}}`);
-  assert.equal(this.$().text(), '2 ocelots');
-});
+  test("pluralize - count 2", async function(assert) {
+    this.set('singular', 'ocelot');
+    await render(hbs`{{pluralize 2 singular}}`);
+    assert.equal(this.$().text(), '2 ocelots');
+  });
 
-test("pluralize - bound count 1", function(assert) {
-  this.set('count', 1);
-  this.set('singular', 'orange');
-  this.render(hbs`{{pluralize count singular}}`);
-  assert.equal(this.$().text(), '1 orange');
-});
+  test("pluralize - bound count 1", async function(assert) {
+    this.set('count', 1);
+    this.set('singular', 'orange');
+    await render(hbs`{{pluralize count singular}}`);
+    assert.equal(this.$().text(), '1 orange');
+  });
 
-test("pluralize - bound count 1, without count", function(assert) {
-  this.set('count', 1);
-  this.set('singular', 'orange');
-  this.set('without-count', true);
-  this.render(hbs`{{pluralize count singular without-count=without-count}}`);
-  assert.equal(this.$().text(), 'orange');
-});
-
-
-test("pluralize - bound count 1.0 float", function(assert) {
-  this.set('count', 1.0);
-  this.set('singular', 'owl');
-  this.render(hbs`{{pluralize count singular}}`);
-  assert.equal(this.$().text(), '1 owl');
-});
-
-test("pluralize - bound count 1.0 float, without count", function(assert) {
-  this.set('count', 1.0);
-  this.set('singular', 'owl');
-  this.set('without-count', true);
-  this.render(hbs`{{pluralize count singular without-count=without-count}}`);
-  assert.equal(this.$().text(), 'owl');
-});
+  test("pluralize - bound count 1, without count", async function(assert) {
+    this.set('count', 1);
+    this.set('singular', 'orange');
+    this.set('without-count', true);
+    await render(hbs`{{pluralize count singular without-count=without-count}}`);
+    assert.equal(this.$().text(), 'orange');
+  });
 
 
-test("pluralize - bound count 1.0 float, without-count=false, specifies count and word", function(assert) {
-  this.set('count', 1.0);
-  this.set('singular', 'owl');
-  this.set('without-count', false);
-  this.render(hbs`{{pluralize count singular without-count=without-count}}`);
-  assert.equal(this.$().text(), '1 owl');
-});
+  test("pluralize - bound count 1.0 float", async function(assert) {
+    this.set('count', 1.0);
+    this.set('singular', 'owl');
+    await render(hbs`{{pluralize count singular}}`);
+    assert.equal(this.$().text(), '1 owl');
+  });
+
+  test("pluralize - bound count 1.0 float, without count", async function(assert) {
+    this.set('count', 1.0);
+    this.set('singular', 'owl');
+    this.set('without-count', true);
+    await render(hbs`{{pluralize count singular without-count=without-count}}`);
+    assert.equal(this.$().text(), 'owl');
+  });
 
 
-test("pluralize - bound count 1.5 float", function(assert) {
-  this.set('count', 1.5);
-  this.set('singular', 'owl');
-  this.render(hbs`{{pluralize count singular}}`);
-  assert.equal(this.$().text(), '1.5 owls');
-});
+  test("pluralize - bound count 1.0 float, without-count=false, specifies count and word", async function(assert) {
+    this.set('count', 1.0);
+    this.set('singular', 'owl');
+    this.set('without-count', false);
+    await render(hbs`{{pluralize count singular without-count=without-count}}`);
+    assert.equal(this.$().text(), '1 owl');
+  });
 
 
-test("pluralize - bound count 1.5 float, without count", function(assert) {
-  this.set('count', 1.5);
-  this.set('singular', 'owl');
-  this.set('without-count', true);
-  this.render(hbs`{{pluralize count singular without-count=without-count}}`);
-  assert.equal(this.$().text(), 'owls');
-});
+  test("pluralize - bound count 1.5 float", async function(assert) {
+    this.set('count', 1.5);
+    this.set('singular', 'owl');
+    await render(hbs`{{pluralize count singular}}`);
+    assert.equal(this.$().text(), '1.5 owls');
+  });
+
+
+  test("pluralize - bound count 1.5 float, without count", async function(assert) {
+    this.set('count', 1.5);
+    this.set('singular', 'owl');
+    this.set('without-count', true);
+    await render(hbs`{{pluralize count singular without-count=without-count}}`);
+    assert.equal(this.$().text(), 'owls');
+  });
 
 
 
-test("pluralize - bound count 1.0 string", function(assert) {
-  this.set('count', '1.0');
-  this.set('singular', 'owl');
-  this.render(hbs`{{pluralize count singular}}`);
-  assert.equal(this.$().text(), '1.0 owl');
-});
+  test("pluralize - bound count 1.0 string", async function(assert) {
+    this.set('count', '1.0');
+    this.set('singular', 'owl');
+    await render(hbs`{{pluralize count singular}}`);
+    assert.equal(this.$().text(), '1.0 owl');
+  });
 
 
-test("pluralize - bound count 1.0 string, without count", function(assert) {
-  this.set('count', '1.0');
-  this.set('singular', 'owl');
-  this.set('without-count', true);
-  this.render(hbs`{{pluralize count singular without-count=without-count}}`);
-  assert.equal(this.$().text(), 'owl');
-});
+  test("pluralize - bound count 1.0 string, without count", async function(assert) {
+    this.set('count', '1.0');
+    this.set('singular', 'owl');
+    this.set('without-count', true);
+    await render(hbs`{{pluralize count singular without-count=without-count}}`);
+    assert.equal(this.$().text(), 'owl');
+  });
 
-test("pluralize - bound count 1.5 string", function(assert) {
-  this.set('count', '1.5');
-  this.set('singular', 'owl');
-  this.render(hbs`{{pluralize count singular}}`);
-  assert.equal(this.$().text(), '1.5 owls');
-});
+  test("pluralize - bound count 1.5 string", async function(assert) {
+    this.set('count', '1.5');
+    this.set('singular', 'owl');
+    await render(hbs`{{pluralize count singular}}`);
+    assert.equal(this.$().text(), '1.5 owls');
+  });
 
-test("pluralize - bound count 1.5 string, without count", function(assert) {
-  this.set('count', '1.5');
-  this.set('singular', 'owl');
-  this.set("without-count", true);
-  this.render(hbs`{{pluralize count singular without-count=without-count}}`);
-  assert.equal(this.$().text(), 'owls');
-});
+  test("pluralize - bound count 1.5 string, without count", async function(assert) {
+    this.set('count', '1.5');
+    this.set('singular', 'owl');
+    this.set("without-count", true);
+    await render(hbs`{{pluralize count singular without-count=without-count}}`);
+    assert.equal(this.$().text(), 'owls');
+  });
 
-test("pluralize - bound count 2", function(assert) {
-  this.set('count', 2);
-  this.set('singular', 'omnivore');
-  this.render(hbs`{{pluralize count singular}}`);
-  assert.equal(this.$().text(), '2 omnivores');
-});
+  test("pluralize - bound count 2", async function(assert) {
+    this.set('count', 2);
+    this.set('singular', 'omnivore');
+    await render(hbs`{{pluralize count singular}}`);
+    assert.equal(this.$().text(), '2 omnivores');
+  });
 
-test("helpers - pluralize - bound count 2, without count", function(assert) {
-  this.set('count', 2);
-  this.set('singular', 'omnivore');
-  this.set('without-count', true);
-  this.render(hbs`{{pluralize count singular without-count=without-count}}`);
-  assert.equal(this.$().text(), 'omnivores');
+  test("helpers - pluralize - bound count 2, without count", async function(assert) {
+    this.set('count', 2);
+    this.set('singular', 'omnivore');
+    this.set('without-count', true);
+    await render(hbs`{{pluralize count singular without-count=without-count}}`);
+    assert.equal(this.$().text(), 'omnivores');
+  });
 });
