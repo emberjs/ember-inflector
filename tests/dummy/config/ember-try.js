32be0c74d091a6bf6c07a042c853833c45dfd5f0
Chris Manson <chris@manson.ie>
2024-07-10 17:04:17 +0100

    update to v4.12 with ember-cli-update
    # Conflicts:
    #	.github/workflows/ci.yml
    #	README.md
    #	tests/dummy/config/ember-try.js

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -47,6 +47,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -67,24 +75,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
           },
         },
       },
