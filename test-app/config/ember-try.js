265e398e7d3380375333aa0170cd26a7d32d5ff7
Chris Manson <chris@manson.ie>
2024-07-24 15:48:49 +0100

    fix ember try

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,6 +8,61 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0',
+            'ember-cli': '~4.12.0',
+            '@ember/test-helpers': '^2.0.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.0',
+            'ember-cli': '~4.12.0',
+            '@ember/test-helpers': '^2.0.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.0',
+            'ember-cli': '~4.12.0',
+            '@ember/test-helpers': '^2.0.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+            'ember-cli': '~4.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
